8b0a36a1b17e9d9aca911e072d5c49f62a6a6f76
VishwasTamta <vishwastamta007@gmail.com>
2026-06-25 18:01:20 +0530

    Updated API endpoint for issue creation and added HTTP request example

--- issues/urls.py
+++ issues/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/issues/', views.create_issue, name="create_issue")
+    path('api/issues', views.create_issue, name="create_issue")
 ]
